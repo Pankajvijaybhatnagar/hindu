d3df8943a2b6a0bd917c003a023d078eef095389
Pankajvijaybhatnagar <135111336+Pankajvijaybhatnagar@users.noreply.github.com>
2025-05-15 07:45:49 +0530

    updated by paankaj only image left and coure

---
+++ layout/footer.php
@@ -0,0 +1,116 @@
+<!-- START FOOTER -->
+<div class="footer section-padding">
+    <div class="container">				
+        <div class="row">						
+            <!-- About Section -->
+            <div class="col-lg-3 col-sm-6 col-xs-12">
+                <div class="single_footer">
+                    <a href="index.html"><img src="assets/images/all-img/logo.png" alt="Hindustan Council of Secondary Education"></a>         
+                    <p>Hindustan Council of Secondary Education is dedicated to providing quality education that empowers students for a brighter future.</p>
+                </div>
+                <div class="foot_social">
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">YouTube</a></li>
+                    </ul>						
+                </div>				
+            </div><!--- END COL -->						
+            
+            <!-- Courses Section -->
+            <div class="col-lg-3 col-sm-6 col-xs-12">
+                <div class="single_footer">
+                    <h4>Courses</h4>
+                    <ul>
+                        <li><a href="#">Mathematics</a></li>
+                        <li><a href="#">Science</a></li>
+                        <li><a href="#">English Language</a></li>
+                        <li><a href="#">Social Studies</a></li>
+                        <li><a href="#">Computer Science</a></li>
+                        <li><a href="#">History & Civics</a></li>
+                    </ul>
+                </div>
+            </div><!--- END COL -->	
+            
+            <!-- Quick Links Section -->
+            <div class="col-lg-3 col-sm-6 col-xs-12">
+                <div class="single_footer">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="admission.html">Admissions</a></li>
+                        <li><a href="courses.html">Our Courses</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                        <li><a href="faq.html">FAQs</a></li>						
+                        <li><a href="news.html">News & Events</a></li>						
+                    </ul>
+                </div>
+            </div><!--- END COL -->	
+            
+            <!-- Contact Info Section -->
+            <div class="col-lg-3 col-sm-6 col-xs-12">
+                <div class="single_footer">
+                    <h4>Contact Info</h4>
+                    <div class="sf_contact">
+                        <span class="ti-mobile"></span>
+                        <h3>Phone Number</h3>
+                        <p>+91 12345 67890</p>
+                    </div>
+                    <div class="sf_contact">
+                        <span class="ti-email"></span>
+                        <h3>Email Address</h3>
+                        <p>info@hindustancouncil.edu.in</p>
+                    </div>
+                    <div class="sf_contact">
+                        <span class="ti-map"></span>
+                        <h3>Office Address</h3>
+                        <p>New Delhi, India</p>
+                    </div>
+                </div>
+            </div><!--- END COL -->	
+        </div><!--- END ROW -->		
+        
+        <!-- Footer Bottom -->
+        <div class="row fc">
+            <div class="col-lg-6 col-sm-6 col-xs-12">
+                <div class="footer_copyright">
+                    <p>&copy; 2025 Hindustan Council of Secondary Education. All Rights Reserved.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-xs-12">
+                <div class="footer_menu">
+                    <ul>
+                        <li><a href="terms.html">Terms of Use</a></li>
+                        <li><a href="privacy.html">Privacy Policy</a></li>
+                        <li><a href="cookie.html">Cookie Policy</a></li>
+                    </ul>
+                </div>
+            </div><!-- END COL -->
+        </div>				
+    </div><!--- END CONTAINER -->
+</div>
+<!-- END FOOTER -->			
+		
+<!-- Latest jQuery -->
+<script src="assets/js/jquery-1.12.4.min.js"></script>
+<!-- Latest compiled and minified Bootstrap -->
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>			
+<!-- Owl Carousel -->
+<script src="assets/owlcarousel/js/owl.carousel.min.js"></script>	
+<!-- Slicknav -->
+<script src="assets/js/jquery.slicknav.js"></script>			
+<!-- Magnific Popup -->               
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<!-- MixItUp Filter -->
+<script src="assets/js/jquery.mixitup.js"></script>			
+<!-- ScrollTop Control -->
+<script src="assets/js/scrolltopcontrol.js"></script>	
+<!-- PureCounter -->
+<script src="assets/js/purecounter_vanilla.js"></script>			
+<!-- WOW - Reveal Animations When You Scroll -->
+<script src="assets/js/wow.min.js"></script>				
+<!-- Main Scripts -->	
+<script src="assets/js/scripts.js"></script>				
+</body>
+</html>
---
+++ layout/hero.php
@@ -0,0 +1,25 @@
+	<section  id="home" class="home_bg">	
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 col-xs-12 text-center">	
+					<div class="home_content">
+						<h1>Yale is the best choice <br />for everyone!</h1>
+						<p>It is a long established fact that reader will be distracted readable content of a page when.</p>						
+					</div>
+					<div class="home_sb">
+						<form action="#" class="banner_subs">
+							<input type="text" class="form-control home_si" placeholder="Search your course here" required="required">
+							<button type="button" class="subscribe__btn">Search <i class="fa fa-paper-plane-o"></i></button>
+						</form>
+					</div>
+					<div class="home_tag">
+						<span>Popular Topic:</span>
+						<a href="#">Design, </a>
+						<a href="#">Development, </a>
+						<a href="#">Graphics, </a>
+						<a href="#">Marketing </a>
+					</div>		  
+				</div><!-- END COL-->
+			</div><!--- END ROW -->			
+		</div><!--- END CONTAINER -->
+	</section>
